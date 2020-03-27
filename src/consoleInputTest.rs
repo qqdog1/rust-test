301c5fde6296bc232133284a27097ca4c6450057
qqdog1 <qqdog1@yahoo.com.tw>
2020-03-27 23:12:00 +0800

    1. new test

---
+++ src/consoleInputTest.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    while(true) {
+        println!("input anything:");
+
+        io::stdin().read_line(&mut input).expect("Failed to read line.");
+
+        println!("{}", input);
+        // the result string will append on the address if input string let outside of while
+    }
+}
